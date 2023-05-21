48f10d3ac897e2f3cad33fa1f4dcaa4db9c0e7d1
Paras Arya <parasarya289@gmail.com>
2023-05-21 15:32:00 +0530

    added products and categories in api routes

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -7,21 +7,23 @@ import { v4 as uuid } from "uuid";
 
 export const categories = [
   {
-    _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    id:1,
+    name:'Mobile',
+    category:'mobile'
   },
   {
-    _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    id:2,
+    name:'Laptop',
+    category:'laptop'
   },
   {
-    _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    id:3,
+    name:'Accessories',
+    category:'accessories'
+  },
+  {
+    id:4,
+    name:'Watch',
+    category:'watch'
   },
 ];
