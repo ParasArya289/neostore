02f970ec3f6e1b5247a4f1e47d99e82481f03934
Paras Arya <parasarya289@gmail.com>
2023-05-22 15:20:06 +0530

    style: added badge to navbar

--- src/frontend/components/Navbar/Navbar.js
+++ src/frontend/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import { Searchbar } from '../Searchbar/Searchbar';
 import {BsCart} from 'react-icons/bs'
 import {BsBag} from 'react-icons/bs'
 import {BiUser} from 'react-icons/bi'
+import { Badge } from 'react-bootstrap';
 
 export const Navbar = () => {
   // const activeNavStyle=({isActive})=>{
@@ -21,8 +22,9 @@ export const Navbar = () => {
         <Searchbar/>
       </div>
       <div className="navbar-links">
-       <NavLink className="navbar-navlinks" to="/wishlist"><BsBag /></NavLink>
-        <NavLink  className="navbar-navlinks" to="/cart"><BsCart /></NavLink>
+
+       <NavLink className="navbar-navlinks" to="/wishlist"><BsBag /><Badge className="nav-badge">12</Badge></NavLink>
+        <NavLink  className="navbar-navlinks" to="/cart"><BsCart /><Badge className="nav-badge">9</Badge></NavLink>
         <NavLink className="navbar-navlinks" to="/auth"><BiUser /></NavLink>
       </div>
     </nav>
