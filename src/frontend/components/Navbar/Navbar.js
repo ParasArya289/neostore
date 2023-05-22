1cc7019017ca8272b08102594973a4749dde3672
Paras Arya <parasarya289@gmail.com>
2023-05-22 22:17:24 +0530

    style: tweaks in navbar

--- src/frontend/components/Navbar/Navbar.js
+++ src/frontend/components/Navbar/Navbar.js
@@ -1,45 +1,71 @@
-import './Navbar.css';
+import "./Navbar.css";
 
 import { NavLink } from "react-router-dom";
-import { Searchbar } from '../Searchbar/Searchbar';
-import {BsCart} from 'react-icons/bs'
-import {BsBag} from 'react-icons/bs'
-import {BiUser} from 'react-icons/bi'
-import { Badge } from 'react-bootstrap';
-import{motion} from "framer-motion";
-import { useState } from 'react';
+import { Searchbar } from "../Searchbar/Searchbar";
+import { BsCart } from "react-icons/bs";
+import { BsBag } from "react-icons/bs";
+import { BiUser } from "react-icons/bi";
+import { Badge } from "react-bootstrap";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const Navbar = () => {
-  const [test,setTest] = useState(0);
-  // const activeNavStyle=({isActive})=>{
-  //   return{
-
-  //   }
-  // }
+  const [test, setTest] = useState(0);
+  const activeNavStyle = ({ isActive }) => {
+    return {
+      color: isActive ? "grey" : "",
+    };
+  };
   return (
-    <nav className="navbar">
-      <div onClick={()=>setTest(test+1)}>
-        <h4 className="navbar-title">Neostore</h4>
+    <motion.nav
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="navbar"
+    >
+      <div onClick={() => setTest(test + 1)}>
+        <NavLink className="navbar-title-navlink" to="/">
+          <h4 className="navbar-title">Neostore</h4>
+        </NavLink>
       </div>
       <div>
-        <Searchbar/>
+        <Searchbar />
       </div>
       <div className="navbar-links">
+        <NavLink
+          style={activeNavStyle}
+          className="navbar-navlinks"
+          to="/wishlist"
+        >
+          <BsBag />
+          <motion.Badge
+            key={test}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="nav-badge"
+          >
+            {test}
+          </motion.Badge>
+        </NavLink>
 
-       <NavLink className="navbar-navlinks" to="/wishlist"><BsBag /><motion.Badge 
-        key={test}
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}className="nav-badge">{test}</motion.Badge></NavLink>
-        
-        <NavLink className="navbar-navlinks" to="/cart"><BsCart /><motion.Badge 
-        key={test}
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}className="nav-badge">{test}</motion.Badge></NavLink>
-        
-        <NavLink className="navbar-navlinks" to="/auth"><BiUser /></NavLink>
+        <NavLink style={activeNavStyle} className="navbar-navlinks" to="/cart">
+          <BsCart />
+          <motion.Badge
+            key={test}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="nav-badge"
+          >
+            {test}
+          </motion.Badge>
+        </NavLink>
+
+        <NavLink style={activeNavStyle} className="navbar-navlinks" to="/auth">
+          <BiUser />
+        </NavLink>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
