91d1364db4821124e1ad22b33ebe4f3f0a8a2a12
Paras Arya <parasarya289@gmail.com>
2023-05-24 01:22:31 +0530

    feat: added filter categories on sidebar

--- src/frontend/components/SideBar/SideBar.js
+++ src/frontend/components/SideBar/SideBar.js
@@ -1,22 +1,46 @@
-import { useState } from "react";
-import './SideBar.css'
+import { useContext, useState } from "react";
+import "./SideBar.css";
 import { Button, Offcanvas } from "react-bootstrap";
-
-export const SideBar = ({showSidebar,handleClose}) => {
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+import { dataContext } from "../../contexts/dataContext";
+export const SideBar = ({ showSidebar, handleClose }) => {
+  const [rating, setRating] = useState(0);
+  const { categories } = useContext(dataContext);
   return (
     <>
       <Offcanvas className="offcanvas" show={showSidebar} onHide={handleClose}>
-
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Filter</Offcanvas.Title>
         </Offcanvas.Header>
 
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-          <Button className="Button"  variant="dark">Clear Filter</Button>
+          <label>
+            Price
+            <input type="range" min="0" max="150000" />
+          </label>
+          <label>
+            Category
+            {categories.map(({ id, name }) => (
+              <label key={id}>
+                <input type="checkbox" />
+                {name}
+              </label>
+            ))}
+          </label>
+          <label>
+            Rating
+            <div>
+              {Array(5)
+                .fill(0)
+                .map((_, i) =>
+                  rating > i ? <AiFillStar key={i} onClick={()=>setRating(i+1)}/> : <AiOutlineStar key={i} onClick={()=>setRating(i+1)}/>
+                )}
+            </div>
+          </label>
+          <Button className="Button" variant="dark">
+            Clear Filter
+          </Button>
         </Offcanvas.Body>
-
       </Offcanvas>
     </>
   );
