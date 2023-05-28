5c106256d6aa69892433bdf99c100257ca8c33db
Paras Arya <parasarya289@gmail.com>
2023-05-29 00:16:57 +0530

    feat: added a logout handler

--- src/frontend/pages/UserProfile/UserProfile.js
+++ src/frontend/pages/UserProfile/UserProfile.js
@@ -3,9 +3,15 @@ import { authContext } from "../../contexts/authContext";
 
 export const UserProfile = () => {
   const { user, token, setToken, setUser } = useContext(authContext);
+  const logoutHandler = () => {
+    setToken("");
+    setUser("");
+    localStorage.clear()
+  }
   return (
     <>
       <h1>Welcome, {user.firstName}</h1>
+      <button onClick={logoutHandler}>Logout</button>
     </>
   );
 };
