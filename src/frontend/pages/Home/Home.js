d0e33b72fb4aa18b654ebb5e0b9f278e63b24a35
Paras Arya <parasarya289@gmail.com>
2023-06-07 13:52:00 +0530

    feat: added home page and home category page

--- src/frontend/pages/Home/Home.js
+++ src/frontend/pages/Home/Home.js
@@ -1,10 +1,50 @@
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Home.css";
+import { motion } from "framer-motion";
+import { HomeCategories } from "./HomeCategories";
+import { Button } from "react-bootstrap";
 export const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <h1>Home</h1>
-      <NavLink to="/products">products</NavLink>
+      <div className="home-container">
+        <motion.div
+          className="gradient-bg"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        ></motion.div>
+        <div className="banner">
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Welcome to Our Store
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            Discover the latest gadget in tech.
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.3 }}
+          >
+            <Button
+              variant="secondary"
+              className="home-btn"
+              onClick={() => navigate("/products")}
+            >
+              Discover
+            </Button>
+          </motion.div>
+        </div>
+      </div>
+      <HomeCategories />
     </>
   );
 };
---
+++ src/frontend/pages/Home/HomeCategories.js
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { useData } from "../../contexts/dataContext";
+import "./Home.css";
+
+export const HomeCategories = () => {
+  const {
+    dataState: { categories },
+  } = useData();
+  const navigate = useNavigate();
+  return (
+    <div className="categories-container">
+      <h2>Categories</h2>
+      <div className="categories">
+        {categories.map((category) => (
+          <motion.div
+            onClick={() => navigate(`/products?category=${category.category}`)}
+            key={category.id}
+            className="category"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h3>{category.name}</h3>
+            <p>{category.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
