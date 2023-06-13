a72363ffc801c3de6d600df75e303df8e258900a
Paras Arya <parasarya289@gmail.com>
2023-06-13 15:47:21 +0530

    style: added animation

--- src/frontend/pages/Checkout/Checkout.js
+++ src/frontend/pages/Checkout/Checkout.js
@@ -3,20 +3,31 @@ import { useNavigate } from "react-router-dom";
 import { AddressList } from "../../components/Address/AddressList";
 import { CheckoutSummary } from "../../components/CheckoutSummary/CheckoutSummary";
 import { useData } from "../../contexts/dataContext";
+import { motion } from "framer-motion";
 export const Checkout = () => {
   const {
     dataState: { address, cart, selectedAddress },
     dataDispatch,
   } = useData();
   const navigate = useNavigate();
-
-  useEffect(() => {
-  }, []);
+  const isMobile = window.innerWidth <= 1000;
+  useEffect(() => {}, []);
   cart.length === 0 && navigate("/products");
 
   return (
-    <div className="cart-container">
-      <div className="cart-items">
+    <motion.div
+      className="cart-container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.div
+        initial={{ x: isMobile ? 0 : 100, y: isMobile ? 60 : 0 }}
+        animate={{ x: 0, y: 0 }}
+        transition={{ duration: 0.2 }}
+        className="cart-items"
+      >
         <div className="cart-center-item">
           <AddressList
             address={address}
@@ -24,10 +35,10 @@ export const Checkout = () => {
             selectedAddress={selectedAddress}
           />
         </div>
-      </div>
+      </motion.div>
       <div className="cart-detail">
         <CheckoutSummary cart={cart} selectedAddress={selectedAddress} />
       </div>
-    </div>
+    </motion.div>
   );
 };
